3060c66ed23dc2e03f1f77fd42b6e2bbd6b4d90a
Yavar Sabbaghan <vantagelogic@outlook.com>
2026-06-05 20:43:39 -0700

    new version of Vantage Logic

--- vantage_logic_api/main.py
+++ vantage_logic_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi import FastAPI, HTTPException, Depends, Request, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
